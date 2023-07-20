c741804482dba7bfa3cc4a5db99dfcf7ae5948cf
jiguotong <1776220977@qq.com>
2023-07-20 19:43:24 +0800

    [Update]修改发布脚本

--- x64/Release/ReleaseRemindMe.bat
+++ x64/Release/ReleaseRemindMe.bat
@@ -1,9 +1,15 @@
 C:\Qt\Qt5.14.1\5.14.1\msvc2017_64\bin\windeployqt.exe RemindMe.exe
-xcopy .\*.dll                        .\RemindMe\    /C /Y
-xcopy .\RemindMe.exe              .\RemindMe\    /C /Y
-xcopy .\iconengines                 .\RemindMe\iconengines\     /C /E /H /Y
-xcopy .\imageformats                 .\RemindMe\imageformats\     /C /E /H /Y
-xcopy .\platforms                 .\RemindMe\platforms\     /C /E /H /Y
-xcopy .\styles                 .\RemindMe\styles\     /C /E /H /Y
-xcopy .\translations                 .\TextEdiRemindMetor\translations\     /C /E /H /Y
-"C:\Program Files\WinRAR\WinRAR.exe" a ".\RemindMe.zip" ".\RemindMe"  /Y
+xcopy .\*.dll                        .\RemindMeApp\    /C /Y
+xcopy .\RemindMe.exe                 .\RemindMeApp\    /C /Y
+
+xcopy .\audio                        .\RemindMeApp\audio\     /C /E /H /Y
+xcopy .\bearer                       .\RemindMeApp\bearer\     /C /E /H /Y
+xcopy .\iconengines                  .\RemindMeApp\iconengines\     /C /E /H /Y
+xcopy .\imageformats                 .\RemindMeApp\imageformats\     /C /E /H /Y
+xcopy .\mediaservice                 .\RemindMeApp\mediaservice\     /C /E /H /Y
+xcopy .\playlistformats              .\RemindMeApp\playlistformats\     /C /E /H /Y
+xcopy .\platforms                    .\RemindMeApp\platforms\     /C /E /H /Y
+xcopy .\styles                       .\RemindMeApp\styles\     /C /E /H /Y
+xcopy .\translations                 .\RemindMeApp\translations\     /C /E /H /Y
+xcopy ..\..\font                     .\RemindMeApp\font\     /C /E /H /Y
+"C:\Program Files\WinRAR\WinRAR.exe" a ".\RemindMeApp.rar" ".\RemindMeApp"  /Y
